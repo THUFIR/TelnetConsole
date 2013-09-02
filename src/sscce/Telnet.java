b18835fffdfeeb87614a9589f9e024c1c70c9cc1
thufir <thufir@dur.bounceme.net>
2013-09-01 21:57:52 -0700

    increment

--- src/sscce/Telnet.java
+++ src/sscce/Telnet.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 import org.apache.commons.net.telnet.TelnetClient;
 
